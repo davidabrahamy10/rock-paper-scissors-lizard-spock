bd9a81860ddddbca85c2d43d5ed3d3f446dea7c2
David Abraham <davidabrahamy10@gmail.com>
2022-10-13 22:34:22 +0000

    Add eventlistener for buttons and DOM loading

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/** Waits for the DOM to load before running the game
+ * and adds eventlisteners to the buttons.
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+})
