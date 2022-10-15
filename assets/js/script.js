e2deee6c7e827a40b49bcb0ba30527ab3c71e04c
David Abraham <davidabrahamy10@gmail.com>
2022-10-15 12:04:27 +0000

    Add eventlisteners for buttons and add reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-choice") === "0") {
+                alert("You chose rock!");
+            } else {
+                let playerChoice = this.getAttribute("data-choice");
+                alert(`You chose ${playerChoice}`);
+            }
+        })
+    }
 })
