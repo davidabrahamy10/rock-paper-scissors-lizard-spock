515fb6a2d0ebbc29a8cd0be29285d80ded71d014
David Abraham <davidabrahamy10@gmail.com>
2022-10-16 09:01:44 +0000

    Declare constants for DOM elements and possible choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+/**
+ * Declare constants for DOM elements
+ * and possible choices
+ */
+ const messages = document.getElementById("choice-message");
+ const buttons = document.getElementsByClassName("control-btn");
+ const playerScoreSpan = document.getElementById("player-score");
+ const computerScoreSpan = document.getElementById("computer-score");
+ const drawScoreSpan = document.getElementById("draw-score");
+ const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+ const resetButton = document.getElementById("reset-btn");
+ 
+ let playerScore = 0;
+ let computerScore = 0;
+ let drawScore = 0;
+
 /** Waits for the DOM to load before running the game
  * and adds eventlisteners to the buttons.
  */
