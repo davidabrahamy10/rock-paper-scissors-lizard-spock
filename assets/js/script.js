fef317f235fc5e9f0f64848f4953038a9ede6423
David Abraham <davidabrahamy10@gmail.com>
2022-10-16 08:32:43 +0000

    Remove logo and add subheading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,14 @@
-/**
- * Declare constants for DOM elements
- * and possible choices
- */
-const messages = document.getElementById("choice-messages");
-const buttons = document.getElementsByTagName("control");
-const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
-const choices = ["rock", "paper", "scissors", "lizard", "spock"];
-
 /** Waits for the DOM to load before running the game
  * and adds eventlisteners to the buttons.
  */
-document.addEventListener("DOMContentLoaded", function() {
+
+ document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-choice") === "0") {
-                .innerHTML = "You chose Rock!";
+                alert("You chose rock!");
             } else {
                 let playerChoice = this.getAttribute("data-choice");
                 alert(`You chose ${playerChoice}`);
@@ -24,7 +16,3 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 })
-
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 5);
-}
